7283310db3e8e116aa1daff7263f6c6334e86ba2
Mabubakar701 <20077868@mydbs.ie>
2025-12-16 19:13:04 +0000

    first

--- tests/test_integration_routes.py
+++ tests/test_integration_routes.py
@@ -458,4 +458,4 @@ class TestIntegrationRoutes(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
--- tests/test_unit_db.py
+++ tests/test_unit_db.py
@@ -289,4 +289,4 @@ class TestDataValidation(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(verbosity=2)
